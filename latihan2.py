91c9c7765916964af70b325b7f2045f7d2e511c4
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-07-24 17:24:54 +0700

    [LEARN] belajar membuat list array

---
+++ latihan2.py
@@ -0,0 +1,12 @@
+# Kotak dengan Banyak Ruang (List)
+tools_favorit = ["Vscode", "Github", "Brave", "Chrome"]
+
+# menampilkan semua daftar
+print("Tools favorit saya adalah", tools_favorit)
+
+# menampilkan tool pertama
+print("Tool pertama dalam daftar adalah:", tools_favorit[0])
+
+# menambahkan tool baru
+tools_favorit.append("Spotify")
+print("Tools favorit saya keseluruhan adalah", tools_favorit)
