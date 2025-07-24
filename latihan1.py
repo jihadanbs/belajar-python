4cb9f0cfb6db77d2c4dab7beacb060d81af85fa1
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-07-24 17:23:52 +0700

    [LEARN] belajar variable dan tipe data dasar

---
+++ latihan1.py
@@ -0,0 +1,8 @@
+# Variabel & Tipe Data Dasar
+nama = "Jihadan Beckhianosyuhada"
+umur = 21
+pemula = True
+
+print("Nama saya adalah", nama)
+print("Umur saya adalah", umur)
+print("Apakah saya pemula?", pemula)
