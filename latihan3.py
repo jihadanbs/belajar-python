e5997fc17685764264cc4aeba66009b391753791
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-07-24 17:25:42 +0700

    [LEARN] belajar dictionary label

---
+++ latihan3.py
@@ -0,0 +1,14 @@
+# Data dengan Label (Dictionary)
+biodata_saya = {
+    "nama": "Jihadan Beckhianosyuhada",
+    "pekerjaan": "Freelancer",
+    "umur": 21,
+    "lokasi": "Surakarta",
+}
+
+print("Menampilkan seluruh data", biodata_saya)
+print("Pekerjaan saya adalah", biodata_saya["pekerjaan"])
+
+# Menambahkan data
+biodata_saya["hobi"] = "Bermain Game"
+print("Menampilkan data baru", biodata_saya)
