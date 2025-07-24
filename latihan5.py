e346970bdc7118239b80094da4b1e84fd6951b91
Jihadan Beckhianosyuhada <jihadanbs11@gmail.com>
2025-07-24 21:47:58 +0700

    [LEARN] belajar looping for

---
+++ latihan5.py
@@ -0,0 +1,7 @@
+# Mengulang Pekerjaan (Looping dengan for)
+daftar_malware = ["WannaCry", "Stuxnet", "ILOVEYOU", "CryptoLocker"]
+
+for malware in daftar_malware:
+    print("Menganalisis malware:", malware)
+
+print("Selesai menganalisis semua malware")
